7923f42756b3d0c1bc3b93e4a0ac347329dbdb06
fuleigesi <107848007+fuleigesi@users.noreply.github.com>
2024-08-30 14:41:15 +0800

    Update basic-usage.vue

--- docs/examples/select-v2/basic-usage.vue
+++ docs/examples/select-v2/basic-usage.vue
@@ -31,7 +31,7 @@ const initials = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
 const value = ref()
 const options = Array.from({ length: 1000 }).map((_, idx) => ({
   value: `Option ${idx + 1}`,
-  label: `${initials[idx % 10]}${idx}`,
+  label: `${initials[idx % 100]}${idx}`,
 }))
 </script>
 
